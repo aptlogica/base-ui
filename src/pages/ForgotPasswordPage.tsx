5a80140c5a81fe1c6c12ac2b3a6a595abebbd506
VirajNawar123 <viraj.nawar@aptlogica.com>
2026-02-04 15:54:54 +0530

    FIxed sonar qube issue for Forgotpassword page component

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -61,7 +61,7 @@ const ForgotPasswordPage: React.FC = () => {
     }
     
     // Check TLD (last part) has at least 2 characters
-    const tld = domainParts[domainParts.length - 1];
+    const tld = domainParts.at(-1);
     if (tld?.length < 2) {
       return false;
     }
