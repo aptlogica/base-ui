586a60fbd65b9f3032f80770b70540bb6eaf7f20
VirajNawar123 <viraj.nawar@aptlogica.com>
2026-06-30 12:17:09 +0530

    updated configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -29,5 +31,4 @@ export default [
         { allowConstantExport: true },
       ],
     },
-  },
-]
+  }, ...storybook.configs["flat/recommended"]];
